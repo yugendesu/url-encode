8ba6c6e692f9f9452f41c5b754d101adf1c041ac
Lichtuser <108939863+Lichtuser@users.noreply.github.com>
2022-07-08 22:12:27 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -61,7 +61,7 @@ async def dl_link(event):
     kk = dl.split("/")[-1]
     aa = kk.split(".")[-1]
     rr = "encode"
-    bb = kk.replace(f".{aa}", "- yugen.mkv")
+    bb = kk.replace(f".{aa}", ".mkv")
     out = f"{rr}/{bb}"
     thum = "thumb.jpg"
     dtime = ts(int((es - s).seconds) * 1000)
@@ -200,7 +200,7 @@ async def encod(event):
         kk = dl.split("/")[-1]
         aa = kk.split(".")[-1]
         rr = f"encode"
-        bb = kk.replace(f".{aa}", "-Yugen.mkv")
+        bb = kk.replace(f".{aa}", ".mkv")
         out = f"{rr}/{bb}"
         thum = "thumb.jpg"
         dtime = ts(int((es - s).seconds) * 1000)
